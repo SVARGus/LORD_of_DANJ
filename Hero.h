f8078b7e975a036f9c291e415773d94584cf2b0e
SVARGus <kyznetsovpn@mail.ru>
2024-09-24 17:54:51 +0300

    Начало работы над классами MainCharacteristic и Hero и продумывание способа сохранения и загрузки игры. Также надо выделить деньги в отдельный класс

---
+++ Hero.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <iostream>
+#include "MainCharacteristic.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+class Hero : public MainCharacteristic
+{
+	int freePoints{};
+	int scalExp{};
+	int scalExpUp{};
+	int winBattle{};
+	int openLvlDanj{};
+	//# InventoryWeapon : array<Weapon, 2> // добавить после реализации классов с оружием
+	//# inventory : Inventory / добавить после реализации класса Инвентарь и предметов (Items) и соответственно оружия и амуниции
+	int Money{}; // позже вывести в отдельный класс для возможности взаимодействия с Магазином,Таверной, выпадение с монстров и других возможностей в будущем
+	
+public:
+	// friend class MainMenu Объявляется как дружественный класс для возможности загрузки и сохранения героя в классе MainMenu без нарушения принципа инкапсуляции
+	// Это позволить MainMenu напрямую работать с приватными членами класса Hero. Возможно стоит также сделать в других классах его дружественным,
+	// если от туда будут записываться данные
+};
+
---
+++ MainCharacteristic.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+class MainCharacteristic
+{
+protected:
+	string name{}; // Имя
+	int lvl{}; // Уровень
+	int pover{}; // Сила
+	int dexterity{}; // Ловкость
+	int endurance{}; // Выносливость
+	int intellogence{}; // интелект
+	int wisdon{}; // Мудрость
+	int charizma{}; // Харизма
+	int minDamage{}; // минимальный урон
+	int maxDamage{}; // максимальный урон
+	double parrying{}; // уклон от атаки
+	double initiative{}; // Инициатива в бою
+	int health{}; // здоровье
+	// Позже дописать дополнительне характеристики которые будут общие для игрока и монстров
+public:
+	virtual ~MainCharacteristic() = 0; // чисто вирутайльный деструктор
+	virtual void recalculateCharacteristic() = 0; // расчет и пересчет характеристик, каждый наследник сам реализует
+	virtual void displayCharacteristic() const = 0; // Вывод характеристик на экран, каждый наследник сам реализует
+	virtual int attack() = 0; // Расчет выдаваемого урона в битве
+	virtual void takeDamage(int attack) = 0; // Расчет получаемого урона и расчет понижения с учетом защиты и других факторов, attack - это метод расчета урона противником
+	// Геттеры переменных
+	string getName() const { return name; }
+	int getLvl() const { return lvl; }
+	int getPover() const { return pover; }
+	int getDexterity() const { return dexterity; }
+	int getEndurance() const { return endurance; }
+	int getIntellogence() const { return intellogence; }
+	int getWisdon() const { return wisdon; }
+	int getMinDamage() const { return minDamage; }
+	int getMaxDamage() const { return maxDamage; }
+	double getParrying() const { return parrying; }
+	double getInitiative() const { return initiative; }
+	int getHealth() const { return health; }
+};
+
