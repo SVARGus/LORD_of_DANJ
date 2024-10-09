bcbefd121f6a95aa0583d016cd8fd5373b2f237e
SVARGus <kyznetsovpn@mail.ru>
2024-10-09 17:37:16 +0300

    Работа над классом Weapon  и подклассами, продумывание механики реализации

--- Weapon.h
+++ Weapon.h
@@ -8,18 +8,32 @@ using std::cin;
 using std::string;
 // Класс интерфейс Оружия. Также тут будут прописаны конкретные виды Оружия (Меч, Дубина, копье). Виды оружия позже расширятся
 // Бует задействован паттерн Абстрактная фабрика
-class Weapon : public Items
+class Weapon : protected Items
 {
-protected:
-	string name{};
-	int damage{};
-	int money{}; // Позже будет заменен на отдельный класс с ценами продажи и покупки
-	string description{};
-	// Прочие характеристики
 public:
+	enum TypeWeapon {SWORD, SPEAR, CLIB}; // Типы оружия: Мечь, Копье, Дубина
+	enum QualityWeapon {RUST, NORMAL, QUALITY, SHARPENED}; // Качество оружия: Ржавое (гнилое), Обычное, Качественное, Заточенное
+	enum TypeDamageWeapon {CUTTING, STABBING, CRUSHING}; // Тип урона: Режущий, Колющий, Дробящий
+protected:
+	TypeWeapon typeWeapon{}; // Для
+	QualityWeapon qualityWeapon{};
+	TypeDamageWeapon typeDamegeWeapon{};
+	int damage{}; // стандарнтый уровень урона будет расчитываться от QualityWeapon и TypeDamageWeapon
+	int money{}; // Позже будет заменен на отдельный класс с ценами продажи и покупки
+	string description{}; // Описание оружия
+	// Прочие характеристики
+
+	void setTypeWeapon(TypeWeapon newTypeWeapon) { typeWeapon = newTypeWeapon; } // возможно не понадобится
+	void setQualityWeapon(QualityWeapon newQualityWeapon) { qualityWeapon = newQualityWeapon; }
+public:
+	virtual ~Weapon() {};
 	virtual int attack() const = 0;
+
 	string getName(){ return name; };
 	int getDamage(){ return damage; };
+	//virtual int calculateAttackDamage() = 0; // Калькулятор урона
+	void setTypeDamageWeapon(TypeDamageWeapon newTypeDamageWeapon) { typeDamegeWeapon = newTypeDamageWeapon; } // Оставим открытым, чтоб во время боя выирать тип удара
+
 };
 
 //Абстрактная фабрика Weapon
@@ -34,9 +48,20 @@ public:
 class Sword : public Weapon
 {
 public:
-	Sword(){}
+	Sword() {}
 	void use() const override {}
-	int attack() const override {}
+	int attack() const override {
+		double finishDamage{};
+		// реализовать расчеты через switch!!
+		/*if (typeDamegeWeapon == CUTTING)
+			return damage;
+		else if (typeDamegeWeapon == STABBING)
+			return damage * 0.8;
+		else if (typeDamegeWeapon == CRUSHING)
+			return damage * 0.5;
+		else
+			return 0;*/
+	}
 };
 class SwordFactory : public WeaponFactory
 {
